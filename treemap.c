60e6539258a3408f30f702de4605be77260a5285
ahkferrari <>
2022-04-22 16:21:57 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -36,44 +36,6 @@ TreeNode * createTreeNode(void* key, void * value) {
     return new;
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "treemap.h"
-
-typedef struct TreeNode TreeNode;
-
-
-struct TreeNode {
-    Pair* pair;
-    TreeNode * left;
-    TreeNode * right;
-    TreeNode * parent;
-};
-
-struct TreeMap {
-    TreeNode * root;
-    TreeNode * current;
-    int (*lower_than) (void* key1, void* key2);
-};
-
-int is_equal(TreeMap* tree, void* key1, void* key2){
-    if(tree->lower_than(key1,key2)==0 &&  
-        tree->lower_than(key2,key1)==0) return 1;
-    else return 0;
-}
-
-
-TreeNode * createTreeNode(void* key, void * value) {
-    TreeNode * new = (TreeNode *)malloc(sizeof(TreeNode));
-    if (new == NULL) return NULL;
-    new->pair = (Pair *)malloc(sizeof(Pair));
-    new->pair->key = key;
-    new->pair->value = value;
-    new->parent = new->left = new->right = NULL;
-    return new;
-}
-
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
   TreeMap * nuevo = (TreeMap *)malloc(sizeof(TreeMap));
