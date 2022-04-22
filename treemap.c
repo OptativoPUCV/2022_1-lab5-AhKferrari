143c65034a1f30d4faaae8ff92912d27ce218816
ahkferrari <>
2022-04-22 16:21:30 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -36,6 +36,44 @@ TreeNode * createTreeNode(void* key, void * value) {
     return new;
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "treemap.h"
+
+typedef struct TreeNode TreeNode;
+
+
+struct TreeNode {
+    Pair* pair;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode * parent;
+};
+
+struct TreeMap {
+    TreeNode * root;
+    TreeNode * current;
+    int (*lower_than) (void* key1, void* key2);
+};
+
+int is_equal(TreeMap* tree, void* key1, void* key2){
+    if(tree->lower_than(key1,key2)==0 &&  
+        tree->lower_than(key2,key1)==0) return 1;
+    else return 0;
+}
+
+
+TreeNode * createTreeNode(void* key, void * value) {
+    TreeNode * new = (TreeNode *)malloc(sizeof(TreeNode));
+    if (new == NULL) return NULL;
+    new->pair = (Pair *)malloc(sizeof(Pair));
+    new->pair->key = key;
+    new->pair->value = value;
+    new->parent = new->left = new->right = NULL;
+    return new;
+}
+
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
   TreeMap * nuevo = (TreeMap *)malloc(sizeof(TreeMap));
@@ -49,8 +87,28 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
 
+  if(tree == NULL)return;
+  
+  TreeNode * aux = tree->root;
+  TreeNode * padre = NULL;
 
-        
+  while(aux){
+    padre = aux;
+    if(tree->lower_than(key, aux->pair->key) == 1)
+    aux = aux->left;
+    
+   
+    else(tree->lower_than( aux->pair->key, key) == 1)
+    aux = aux->right;
+    
+    //return; 
+  }
+
+  TreeNode * nodo = createTreeNode(key,value);
+  if(tree->lower_than(key, aux->pair->key) == 1)
+    padre->left = nodo;
+  else(tree->lower_than( aux->pair->key, key) == 1)
+    padre->right = nodo;
 }
 
 TreeNode * minimum(TreeNode * x){
