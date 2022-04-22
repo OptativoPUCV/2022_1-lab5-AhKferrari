096c1d32ccf392e8f9e1f93f6dadf6c23eeffffc
ahkferrari <>
2022-04-22 15:19:41 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -56,7 +56,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
 }
 
 TreeNode * minimum(TreeNode * x){
-  TreeNode * aux = tree->pair->root;
+  TreeNode * aux = x->pair->root;
   while(aux->left != NULL){
     aux = aux->left;
   }
