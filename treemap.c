dc02f841fa721867e3a6fd37c72697f4d211da4f
ahkferrari <>
2022-04-22 16:30:25 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -60,7 +60,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     aux = aux->left;
     else if(tree->lower_than(aux->pair->key,key)){
     aux = aux->right;
-      else{
+      } else{
         return;
       }
     }
