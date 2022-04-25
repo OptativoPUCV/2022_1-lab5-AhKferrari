dcf987acb2232f419eaa46713338509a344ba15a
ahkferrari <>
2022-04-25 21:24:33 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -107,16 +107,16 @@ void removeNode(TreeMap * tree, TreeNode* node) {
       if(node->left != NULL){
         node->parent->left = node->left->parent;
         node->left->parent = node->parent->left;
-        node=NULL; }
-        //free(node);
+        node=NULL; 
+        free(node);}
       return;}
 
     if(node->left == NULL && node->right != NULL){
       if(node->right != NULL){
         node->parent->left = node->right->parent;
         node->right->parent = node->parent->left;
-        node=NULL; }
-        //free(node);
+        node=NULL; 
+        free(node);}
       return;}
   }
 
@@ -124,16 +124,16 @@ void removeNode(TreeMap * tree, TreeNode* node) {
      if(node->left != NULL){
         node->parent->right = node->left->parent;
         node->left->parent = node->parent->right;
-        node=NULL; }
-        //free(node);
+        node=NULL; 
+        free(node)}
       return;}
 
     if(node->left == NULL && node->right != NULL){
       if(node->right != NULL){
         node->parent->right = node->right->parent;
         node->right->parent = node->parent->right;
-        node=NULL; }
-        //free(node);
+        node=NULL; 
+        free(node)}
       return;}
 
   }
