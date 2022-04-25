84620914a51abf77c2cf32e05309b3349272bb67
ahkferrari <>
2022-04-25 22:28:44 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -207,13 +207,13 @@ Pair * nextTreeMap(TreeMap * tree) {
     }
     return aux->pair;
     }
-  *else{
+  /*else{
    while(aux->key < tree->current->key){
      if(aux->parent->key < tree->current->key){
        aux->parent->key = aux->parent->parent->key;
      }
    else return aux->pair;} 
-  } 
+  } */
   
   return NULL;
 }
