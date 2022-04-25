af7dedb87d8f5203f62748dbd1f39172a6b1f17c
ahkferrari <>
2022-04-25 02:35:58 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -161,8 +161,8 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
-  //Pair * first = (Pair *)malloc(sizeof(Pair));  
-  first = minimum(tree*);
+  Pair * first = (Pair *)malloc(sizeof(Pair));  
+  first = minimum(tree);
   return first;
 }
 
