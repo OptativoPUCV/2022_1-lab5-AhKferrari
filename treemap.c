122a0ec257b02669d22bf4a1676af2bdf69feef1
ahkferrari <>
2022-04-25 00:44:27 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -100,7 +100,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
     else node->parent->left = NULL;
     return;
   }
-  /*else if (node->left || node->right){
+  else if (node->left || node->right){
     if(node->left) 
       node->parent->left = node->left;
     if(node->right)
